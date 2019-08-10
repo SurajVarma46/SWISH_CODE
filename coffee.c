ed539578520b81cbfa7d35c8463437676bc07bfe
Suraj Varma Kakarlapudi <sukakarlapudi@gmail.com>
2019-08-10 13:32:02 +0530

    Update coffee.c

--- coffee.c
+++ coffee.c
@@ -3,13 +3,12 @@ int main(void)
 {
   int x,p,a,value=0;
   scanf("%d %d",&x,&p);
-  a=(p/100)*x; 
-  value=value+x
-  while(x-a)
+  a=x*p/100;
+  while(a>=1)
   {
-    a=x-((p/100)*x);
-    x=x-a;
-    value=value+x;
+      value+=x;
+      a=(x*p/100);
+      x=x-a;
   }
   printf("%d",value);
   return 0;
