b153a025a425ee5619d0dd56eea4b61e00d22dc4
Suraj Varma Kakarlapudi <sukakarlapudi@gmail.com>
2019-08-10 13:21:02 +0530

    Update coffee.c

--- coffee.c
+++ coffee.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int main(void)
 {
-  int x,p,count=1,a;
+  int x,p,count=1,a,value=0;
   scanf("%d %d",&x,&p);
   a=(p/100)*x; 
+  value=value+x
   while(x-a)
   {
-    count++;
     a=x-((p/100)*x);
     x=x-a;
+    value=value+x;
   }
-  printf("%d",count);
+  printf("%d",value);
   return 0;
 }
